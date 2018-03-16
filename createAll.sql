5ed3baff4da7377b2a329d9cdaa244b2f5801ce8
Sean Gallagher <paddytobagen@gmail.com>
2018-03-15 17:00:30 -0700

    Add files via upload

---
+++ createAll.sql
@@ -0,0 +1,5 @@
+@ create1
+@ create2
+@ create3
+@ create4
+@ create5
---
+++ insertAll.sql
@@ -0,0 +1,11 @@
+SET DEFINE OFF
+
+@ Year2010insert.sql
+
+@ Year2011insert.sql
+
+@ Year2012insert.sql
+
+@ Year2013insert.sql
+
+@ Year2014insert.sql
